3dfd1b5ae57ed158305e86c513ff88e280e6840c
Alexander2327 <danilov.aa1@mail.ru>
2023-05-05 14:29:46 +0300

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/iauyeq8jjcbvv97e?svg=true)](https://ci.appveyor.com/project/Alexander2327/ws-frontend)
---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import modalLogin from './modal/modalLogin';
+
+const board = document.querySelector('.board');
+const URL = 'http://localhost:9020';
+
+modalLogin(board, URL);
