65f0393844638427b5e1a0f32d098de74bc94cff
Alexander2327 <danilov.aa1@mail.ru>
2023-05-09 11:24:33 +0300

    fix url

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,6 @@ import modalLogin from './modal/modalLogin';
 
 const board = document.querySelector('.board');
 // const URL = 'http://localhost:9020';
-const URL = 'https://ws-backend-jgax.onrender.com/';
+const URL = 'https://ws-backend-jgax.onrender.com';
 
 modalLogin(board, URL);
